1c05f788010bc5751b292cc6a10558ad15079f62
Boyang Niu <boyang.niu@gmail.com>
2022-03-20 17:28:15 -0700

    Without kontour

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,11 +1,9 @@
-require("hardhat-kontour");
-
 module.exports = {
   solidity: "0.8.11",
   paths: {
     sources: "./ethereum",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
-  }
+    artifacts: "./artifacts",
+  },
 };
